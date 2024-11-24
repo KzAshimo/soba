a7ddf4363492df0a00cf88e8ae2eaf527a891d71
KzAshimo <good.ashimo@gmail.com>
2024-11-24 23:25:10 +0900

    first

---
+++ src/main.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>soba</title>
+    <link href="../public/styles.css" rel="stylesheet">
+</head>
+<body class="font-sans m-auto mx-6">
+    <h1 class="text-4xl text-center my-5">OrganizerForm</h1>
+    <section>
+        <form action="" class="text-2xl border-solid border-2 border-slate-600">
+            <h2>現在の金額</h2>
+            <p>￥
+            </p>
+        </form>
+    </section>
+    <section class="border-solid border-2 border-slate-600">
+        <h2 class="text-2xl font-bold">飲み物</h2>
+    <form action="">
+        <h3 class="bg-amber-300 text-xl">ビール(KIRIN)
+        </h3>
+        <div class="flex m-3">
+        <button class="border-solid border-2 border-slate-600 bg-black text-white p-1">中ジョッキ</button>
+        <p class="mx-1 p-1">￥700</p>
+        </div>
+    </form>
+    </section>
+</body>
+</html>
